ef706b7fbbcaa89041aa5c6202aa7cffa36ac99b
Arkadiusz Machalica <a.machalica@selleo.com>
2020-12-08 15:23:44 +0100

    Unoptimized example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
+import MOCK_DATA from "./MOCK_DATA";
 
 function App() {
+  const [list, setList] = useState(MOCK_DATA);
+  const [timer, setTimer] = useState(0);
+
+  useEffect(function timerSetup() {
+    const timerId = setInterval(() => {
+      setTimer((currentTimer) => currentTimer + 1)
+    }, 10)
+
+    return () => clearInterval(timerId)
+  }, [])
+
+  const filteredListElements = list.filter((_, index) => index % 24)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Example big component
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div>Some timer value: {timer}</div>
+
+      <div>
+        <h3>Some big list</h3>
+        <ol style={{width: "40%", margin: '0 auto'}}>
+          {filteredListElements.map((element) => {
+            console.log("mapping")
+            return (
+              <li key={element.id} style={{border: '1px solid black', marginBottom: '1rem'}}>
+                <b>{element.first_name}</b>
+                <b>{element.last_name}</b>
+                {' - '}
+                <span>{element.email}</span>
+                {' - '}
+                <span>{element.gender}</span>
+                <pre>{element.ip_address}</pre>
+              </li>
+            )
+          })}
+        </ol>
+      </div>
     </div>
   );
 }
